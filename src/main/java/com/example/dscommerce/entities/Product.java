a4cfb881ba0205d84916ccb577048e7036e918e5
pcechetto <pedrocechetto@hotmail.com>
2025-02-14 14:52:54 -0300

    OrderItem & OrderItemPK entities

--- src/main/java/com/example/dscommerce/entities/Product.java
+++ src/main/java/com/example/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.example.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -11,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,13 +26,16 @@ public class Product {
 
     @Column(columnDefinition = "TEXT")
     private String description;
-    
+
     private double price;
     private String imgUrl;
 
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -85,5 +90,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
