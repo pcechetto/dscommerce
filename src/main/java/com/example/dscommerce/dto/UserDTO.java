4302782c673bab64786e60c7a8c377308b39a99b
pcechetto <pedrocechetto@hotmail.com>
2025-03-31 13:51:22 -0300

    Get user logged

---
+++ src/main/java/com/example/dscommerce/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package com.example.dscommerce.dto;
+
+import com.example.dscommerce.entities.Role;
+import com.example.dscommerce.entities.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+
+    private List<String> roles = new ArrayList<>();
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        phone = entity.getPhone();
+        birthDate = entity.getBirthDate();
+        roles = entity.getRoles().stream().map(Role::getAuthority).toList();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
